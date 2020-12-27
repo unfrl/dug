a29f260e49e42abf73f7bc945c57fbc2d0f2b643
matt <matt@kaijucode.com>
2020-12-26 17:56:56 -0700

    Now the progress bar background changes from red to green if your console support turecolor... Imma stop messing with the animations now :)

--- Services/PercentageAnimator.cs
+++ Services/PercentageAnimator.cs
@@ -1,6 +1,7 @@
 using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading;
+using Spectre.Console;
 
 namespace dug.Services
 {
@@ -17,6 +18,8 @@ namespace dug.Services
         private int _progressBarLength;
         private object _syncObject = 0;
         private string _customString;
+        private Stopwatch _stopwatch = new Stopwatch();
+        private bool _trueColor = AnsiConsole.Capabilities.Supports(ColorSystem.TrueColor);
 
         public PercentageAnimator (){
             _thread = new Thread(Spin);
@@ -40,6 +43,7 @@ namespace dug.Services
             
             _active = true;
             if (!_thread.IsAlive){
+                _stopwatch.Restart();
                 _thread.Start();
             }
         }
@@ -62,16 +66,28 @@ namespace dug.Services
         private void Draw(char c)
         {
             double progress;
+            TimeSpan elapsed;
             lock(_syncObject){
                 progress = _count/_totalEvents;
+                elapsed= _stopwatch.Elapsed;
             }
             Console.SetCursorPosition(_left, _top);
+
             Console.ForegroundColor = ConsoleColor.Green;
-            
             Console.Write(c);
-            Console.Write(" "+GetProgressBar(progress));
+            if(_trueColor){
+                Console.ForegroundColor = ConsoleColor.White;
+                var progressColor = Interpolate(Color.Red, Color.Green, progress);
+                AnsiConsole.Background = progressColor;
+                AnsiConsole.Write(GetProgressBar(progress));
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else{
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(GetProgressBar(progress));
+            }
             Console.Write(_customString);
-            Console.Write($" {(progress).ToString("P3")} ({_count}/{_totalEvents})");
+            Console.Write($" {(progress).ToString("P3")} ({_count}/{_totalEvents}) ({elapsed.ToString("mm\\:ss\\:fff")})");
             if(!_active){
                 Console.Write(Environment.NewLine);
             }
@@ -98,6 +114,22 @@ namespace dug.Services
         public void Dispose()
         {
             Stop();
+        }
+
+        private Color Interpolate(Color color1, Color color2, double fraction)
+        {
+            double r = InterpolateDoubles(color1.R, color2.R, fraction);
+            double g = InterpolateDoubles(color1.G, color2.G, fraction);
+            double b = InterpolateDoubles(color1.B, color2.B, fraction);
+            return new Color(
+                Convert.ToByte(r),
+                Convert.ToByte(g),
+                Convert.ToByte(b));
+        }
+
+        private double InterpolateDoubles(double d1, double d2, double fraction)
+        {
+            return d1 + (d2 - d1) * fraction;
         }
     }
 }
