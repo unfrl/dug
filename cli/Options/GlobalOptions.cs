4debc51624fa399981e159b61999f32686d4bac4
matt <matt@kaijucode.com>
2021-01-04 18:43:36 -0700

    Imporve -t help text

--- cli/Options/GlobalOptions.cs
+++ cli/Options/GlobalOptions.cs
@@ -13,7 +13,7 @@ namespace dug.Options
         [Option("retries", Required = false, HelpText = "The number of times to retry queries on servers that error (or timeout)", Default = 0)]
         public int QueryRetries { get; set; }
 
-        [Option('t', "timeout", Required = false, HelpText = "The timeout (in ms) to be used when querying the DNS Server(s). If there are multiple it will apply to each server", Default = 3000)]
+        [Option('t', "timeout", Required = false, HelpText = "The timeout (in ms) to be used when querying each DNS Server", Default = 3000)]
         public int Timeout { get; set; }
     }
 }
